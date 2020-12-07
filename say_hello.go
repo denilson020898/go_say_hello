60bc2ceb8ba431bc12835a3ed2f53fb508697fdd
Denilson <denilson020898@gmail.com>
2020-12-07 13:13:36 +0700

    upgrade version

--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	result := "Hello " + name
+func SayHello(msg string, name string) string {
+	result := msg + " " + name
 	return result
 }
